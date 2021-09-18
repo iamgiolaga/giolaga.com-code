ea3ee7a51e876b3804b3e082924f15ee6b3b04c2
Giovanni Laganà <giovanni.lagana@studenti.unimi.it>
2021-09-18 15:27:29 +0200

    removed workInProgress

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import me from './me.png';
-import workInProgress from './workInProgress.png';
 import './App.css';
 
 function App() {
@@ -9,10 +8,6 @@ function App() {
         <div id="presentationContainer">
           <p id="hello">Hi! I am Giovanni :)</p>
           <img id="profilePic" src={me} alt="Profile"/>
-          <div style={{marginTop:"50px"}}>
-            <img src={workInProgress} alt="WorkInProgress" width="70px" height="70px"></img>
-            <div>My personal website is coming soon!</div>
-          </div>
         </div>
       </header>
     </div>
