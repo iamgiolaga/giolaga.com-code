49853725c89942e133406bce2ba92f12d17f9732
Giovanni Laganà <giovanni.lagana@studenti.unimi.it>
2021-09-19 15:35:44 +0200

    history title now has css id

--- src/History/index.js
+++ src/History/index.js
@@ -4,7 +4,7 @@ import CustomizedTimeLine from './CustomizedTimeLine';
 const History = () => {
     return(
         <div id="timelineContainer">
-            <h1>History</h1>
+            <h1 id="historyTitle">History</h1>
             <CustomizedTimeLine />
         </div>
     );
