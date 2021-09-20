b80c5cbc5419b43445d3c8c1854fb313035d412c
Giovanni Laganà <giovanni.lagana@studenti.unimi.it>
2021-09-20 22:10:38 +0200

    fixing scrolling bug

--- src/App.js
+++ src/App.js
@@ -2,18 +2,29 @@ import './App.css';
 import Presentation from './Presentation';
 import Biography from './Biography';
 import History from './History';
+import {Link} from 'react-scroll';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <Presentation />
-      </header>
-      <header className="App-header">
-        <Biography />
-      </header>
-      <section>
-        <History />
+      <nav id="nav-wrap">
+        <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
+        <a className="mobile-btn" href="#" title="Hide navigation">Hide navigation</a>
+        <ul id="nav" class="nav">
+          <li><Link to="presentation" spy={true} smooth={true} duration={1500}><a className="smoothscroll" href="#presentation">Home</a></Link></li>
+          <li><Link to="biography" spy={true} smooth={true} duration={1500}><a className="smoothscroll" href="#biography">Biography</a></Link></li>
+          <li><Link to="history" spy={true} smooth={true} duration={1500}><a className="smoothscroll" href="#history">History</a></Link></li>
+        </ul> 
+      </nav>
+      <section id="presentation" className="App-header">
+          <Presentation />
+      </section>
+      <section id="biography" className="App-header">
+          <Biography />
+      </section>
+      <section id="history" >
+          <History />
       </section>
     </div>
   );
