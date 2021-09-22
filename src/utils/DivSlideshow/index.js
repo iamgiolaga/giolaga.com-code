248452b3df9c164b77d594aed7c4d783c7c0dc56
Giovanni Laganà <giovanni.lagana@studenti.unimi.it>
2021-09-23 00:37:46 +0200

    slideshow for divs

---
+++ src/utils/DivSlideshow/index.js
@@ -0,0 +1,59 @@
+import { useState, useEffect, useRef } from "react";
+import './style.css';
+
+const delay = 5000;
+
+const DivSlideshow = ({elements}) => {
+  const [index, setIndex] = useState(0);
+  const timeoutRef = useRef(null);
+
+  function resetTimeout() {
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current);
+    }
+  }
+
+  useEffect(() => {
+    resetTimeout();
+    timeoutRef.current = setTimeout(
+      () =>
+        setIndex((prevIndex) =>
+          prevIndex === elements.length - 1 ? 0 : prevIndex + 1
+        ),
+      delay
+    );
+
+    return () => {
+      resetTimeout();
+    };
+  }, [index]);
+
+  return (
+    <div className="slideshow">
+      <div
+        className="slideshowSlider"
+        style={{ transform: `translate3d(${-index * 100}%, 0, 0)` }}
+      >
+        {elements.map((el, index) => (
+            <div className="slide" key={index}>
+                {el}
+            </div>
+        ))}
+      </div>
+
+      <div className="slideshowDots">
+        {elements.map((_, idx) => (
+          <div
+            key={idx}
+            className={`slideshowDot${index === idx ? " active" : ""}`}
+            onClick={() => {
+              setIndex(idx);
+            }}
+          ></div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default DivSlideshow;
