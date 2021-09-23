4cc089d762dcafc67b8d9980a60231b5dd9cd749
Giovanni Laganà <giovanni.lagana@studenti.unimi.it>
2021-09-23 23:21:29 +0200

    store domRef.current in a variable

--- src/utils/FadeInSection/index.js
+++ src/utils/FadeInSection/index.js
@@ -8,8 +8,9 @@ const FadeInSection = (props) => {
         const observer = new IntersectionObserver(entries => {
             entries.forEach(entry => setVisible(entry.isIntersecting));
         });
-        observer.observe(domRef.current);
-        return () => observer.unobserve(domRef.current);
+        let curr = domRef.current;
+        observer.observe(curr);
+        return () => observer.unobserve(curr);
     }, []);
 
     return (
