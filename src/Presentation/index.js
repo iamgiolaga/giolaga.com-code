05e88de17cf576537de85ed0c74f27b76573fb8d
Giovanni Laganà <giovanni.lagana@studenti.unimi.it>
2021-09-25 09:54:04 +0200

    added mail icon

--- src/Presentation/index.js
+++ src/Presentation/index.js
@@ -2,6 +2,7 @@ import './style.css';
 import Slideshow from '../utils/Slideshow';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
 
 const Presentation = () => {
     return(
@@ -11,6 +12,7 @@ const Presentation = () => {
           <div >
             <a className="socialLink" href="https://www.linkedin.com/in/giovannilagana/"><LinkedInIcon style={{width:'50px', height: '50px'}} className="socialIcon"/></a>
             <a className="socialLink" href="https://github.com/iamgiolaga"><GitHubIcon style={{width:'50px', height: '50px'}} className="socialIcon"/></a>
+            <div className="mailLink" onClick = {() => window.open('mailto:giovanni.lagana1995@gmail.com')}><MailOutlineIcon style={{width:'50px', height: '50px'}} className="socialIcon"/></div>
           </div>
         </div>
     );
