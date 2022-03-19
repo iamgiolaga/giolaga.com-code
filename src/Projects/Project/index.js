84f598e3a853fd2cecf3d23c671cd5ba05863c68
Giovanni Laganà <giovanni.lagana@studenti.unimi.it>
2022-03-19 15:59:19 +0100

    fixed modals and added x

--- src/Projects/Project/index.js
+++ src/Projects/Project/index.js
@@ -4,6 +4,7 @@ import Backdrop from '@mui/material/Backdrop';
 import Box from '@mui/material/Box';
 import Modal from '@mui/material/Modal';
 import Fade from '@mui/material/Fade';
+import CloseIcon from '@mui/icons-material/Close';
 import {useDispatch} from 'react-redux'
 import {focusOnProject} from '../../store/focus';
 
@@ -23,13 +24,18 @@ const Project = ({
     const [modalStatus, setModalStatus] = useState(false);
     const [hoveredProject, setHoveredProject] = useState(false);
 
+    const handleCloseModal = () => {
+        setModalStatus(false);
+        dispatch(focusOnProject(false));
+    };
+
     const style = {
         position: 'absolute',
         top: '50%',
         left: '50%',
         transform: 'translate(-50%, -50%)',
         width: '90%',
-        height: 'fit-content',
+        height: '530px',
         padding: '2%',
         bgcolor: '#181b20',
         boxShadow: '0 5px 10px rgba(0, 0, 0, 0.3)',
@@ -59,24 +65,25 @@ const Project = ({
                 aria-labelledby="transition-modal-title"
                 aria-describedby="transition-modal-description"
                 open={modalStatus}
-                onClose={() => {
-                    setModalStatus(false);
-                    dispatch(focusOnProject(false));
-                }}
+                id="projectModal"
+                onClose={handleCloseModal}
                 closeAfterTransition
                 BackdropComponent={Backdrop}
                 BackdropProps={{
-                timeout: 500,
+                    timeout: 500,
                 }}
             >
                 <Fade in={modalStatus}>
                     <Box sx={style}>
-                        <div>
+                        <CloseIcon id="closeIcon" onClick={handleCloseModal}></CloseIcon>
+                        <div style={{height: '450px'}}>
                             <h1>{title}</h1>
                             <h3>{subtitle}</h3>
                             <h5>{startDate} - {endDate}</h5>
-                            <div style={{marginTop: '4%'}}>{description}</div>
-                            <p style={{marginTop: '3%'}}><b>Technologies:</b> {technologies}</p>
+                            <div style={{position: 'relative', marginTop: '4%', maxHeight: '60%', overflowY: 'scroll'}}>
+                                <div>{description}</div>
+                            </div>
+                                <p style={{marginTop: '3%'}}><b>Technologies:</b> {technologies}</p>
                         </div>
                     </Box>
                 </Fade>
