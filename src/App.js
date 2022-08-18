fe9e1192c5da79f390c29b4063edb782fe3b7d99
Giovanni Laganà <giovanni.lagana@studenti.unimi.it>
2022-08-18 23:05:15 +0200

    adjusted border-radius

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, {Suspense} from 'react';
+import React, { Suspense } from 'react';
 import './App.css';
-import {Link} from 'react-scroll';
+import { Link } from 'react-scroll';
 import Presentation from './Presentation';
 import Biography from './Biography';
 import History from './History';
@@ -9,6 +9,7 @@ import Skills from './Skills';
 import { useTranslation, withTranslation } from 'react-i18next';
 import { compose } from 'redux';
 import ReactCountryFlag from 'react-country-flag';
+import EmojiEventsIcon from '@mui/icons-material/EmojiEvents';
 
 function App() {
   const { t, i18n } = useTranslation('common');
@@ -16,10 +17,10 @@ function App() {
   return (
     <Suspense fallback="loading">
       <div className="App">
-        <div id="languages" style={{position: 'fixed', float: 'left', marginLeft: '20px', zIndex: '999'}}>
-            <ReactCountryFlag className="languageFlag" onClick={() => i18n.changeLanguage('it')} countryCode="IT" svg style={{ marginTop: '5px', width:'1.80em', height:'1.80em'}}/>
-            <ReactCountryFlag className="languageFlag" onClick={() => i18n.changeLanguage('en')} countryCode="GB" svg style={{ marginTop: '5px', marginLeft: '10px', width:'1.80em', height:'1.80em'}}/>
-        </div> 
+        <div id="languages" style={{ position: 'fixed', float: 'left', marginLeft: '20px', zIndex: '999' }}>
+          <ReactCountryFlag className="languageFlag" onClick={() => i18n.changeLanguage('it')} countryCode="IT" svg style={{ marginTop: '5px', width: '1.80em', height: '1.80em' }} />
+          <ReactCountryFlag className="languageFlag" onClick={() => i18n.changeLanguage('en')} countryCode="GB" svg style={{ marginTop: '5px', marginLeft: '10px', width: '1.80em', height: '1.80em' }} />
+        </div>
         <nav id="nav-wrap">
           <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
           <a className="mobile-btn" href="#nav" title="Hide navigation">Hide navigation</a>
@@ -32,19 +33,19 @@ function App() {
           </ul>
         </nav>
         <section id="presentation" className="App-header">
-            <Presentation />
+          <Presentation />
         </section>
         <section id="biography" className="App-header">
-            <Biography />
+          <Biography />
         </section>
         <section id="history" className="App-header">
-            <History />
+          <History />
         </section>
         <section id="projects">
-            <Projects />
+          <Projects />
         </section>
         <section id="skills">
-            <Skills />
+          <Skills />
         </section>
         <p id="websiteVersion">giolaga.com - v1.3</p>
       </div>
