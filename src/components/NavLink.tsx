f3a1fba53e261b403f6d5b16f311bc36b040aa53
Giovanni Laganà <giovanni.lagana1995@gmail.com>
2026-03-01 21:41:49 +0100

    layout fixes and menu bug

--- src/components/NavLink.tsx
+++ src/components/NavLink.tsx
@@ -3,12 +3,19 @@ import { Link } from 'react-scroll';
 interface NavLinkProps {
   to: string;
   label: string;
+  onLinkClick?: () => void;
 }
 
-const NavLink = ({ to, label }: NavLinkProps) => {
+const NavLink = ({ to, label, onLinkClick }: NavLinkProps) => {
   return (
     <li>
-      <Link to={to} spy={true} smooth={false} className="smoothscroll">
+      <Link 
+        to={to} 
+        spy={true} 
+        smooth={false} 
+        className="smoothscroll"
+        onClick={onLinkClick}
+      >
         {label}
       </Link>
     </li>
